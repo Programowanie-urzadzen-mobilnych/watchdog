acf0b3327ea563bb31aa0f549717bc0e26157390
Suddi <suddenik@gmail.com>
2020-05-22 15:18:39 +0200

    Making use of watchdog file saving exception (file with given name exists) from watchdog, also fixing watchdog new path generation

--- src/main/java/pl/grupa33inf/watchdog/exceptions/ELayoutName.java
+++ src/main/java/pl/grupa33inf/watchdog/exceptions/ELayoutName.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class ELayoutName{
    public static String getNewPath(String directory, String filename, String fileExtennsion)
    {
-       String path = "/storage/emulated/0/";
-       path=path+directory+"/"+filename+"(1)"+"."+fileExtennsion;
+       //TODO: if file with (number) exists, then increase that number.
+       String path=directory+"/"+filename+"(1)"+"."+fileExtennsion;
        return path;
    }
 }
