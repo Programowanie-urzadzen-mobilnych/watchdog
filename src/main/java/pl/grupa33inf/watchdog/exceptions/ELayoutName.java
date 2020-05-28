5b13a414dd42c0e15efd0a19d1ddfb8ceac741d3
unknown <alicjajagoda1997@gmail.com>
2020-05-28 21:12:56 +0200

    SR-78 if file with (number) exists increase number

--- src/main/java/pl/grupa33inf/watchdog/exceptions/ELayoutName.java
+++ src/main/java/pl/grupa33inf/watchdog/exceptions/ELayoutName.java
@@ -5,8 +5,47 @@ import java.io.File;
 public class ELayoutName{
    public static String getNewPath(String directory, String filename, String fileExtennsion)
    {
-       //TODO: if file with (number) exists, then increase that number.
-       String path=directory+"/"+filename+"(1)"+"."+fileExtennsion;
+       int l=filename.length();
+       String path="";
+       String temp="";
+       if(filename.charAt(l-1) == ')')
+       {
+           if(filename.charAt(l-3)=='(') // for one-digit number
+           {
+               String x = changeNumber(2,filename);
+               temp=filename.substring(0,l-3)+'('+x+')';
+               path=directory+"/"+temp+""+"."+fileExtennsion;
+
+           }else if(filename.charAt(l-4)=='(') // for two-digit number
+           {
+               String x = changeNumber(3,filename);
+               temp=filename.substring(0,l-4)+'('+x+')';
+           }else if(filename.charAt(l-5)=='(') // for three-digit number
+           {
+               String x = changeNumber(4,filename);
+               temp=filename.substring(0,l-5)+'('+x+')';
+           }else if(filename.charAt(l-6)=='(') // for four-digit number
+           {
+               String x = changeNumber(5,filename);
+               temp=filename.substring(0,l-6)+'('+x+')';
+           }} else path=directory+"/"+filename+"."+fileExtennsion;
+
        return path;
+   }
+
+   private static String changeNumber(int i, String name)
+   {
+       int num;
+       int l=name.length();
+       String number="";
+       while( i>1 )
+       {
+           number+=name.charAt(l-i);
+           i--;
+       }
+       num = Integer.parseInt(number);
+       num++;
+       number=Integer.toString(num);
+       return number;
    }
 }
